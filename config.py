d851d521472c1c279b24556b2472ba6084662213
itsDev-Developer <148090753+itsDev-Developer@users.noreply.github.com>
2024-06-04 00:00:31 +0530

    Update config.py
    POWER OF OPTION ADDED

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nYou are just 1 step away from joining 'AI TRADING BILLIONAIRE' official telegram channel 🎯🎯🚀🚀\n\nGet Daily FREE trading calls from our expert team having more than 11yrs experience in Nifty, Bank Nifty, FinNifty, Stock Option.\n\n✅ Trusted by 35,000+ traders\n\n✅ Receive 2-4 daily free trade calls and updates\n\n✅ More than 11yrs experience\n\n✅ Join for latest updates\n\n👇 Click on below button to join official 'AI TRADING BILLIONAIRE' telegram channel 👇\n\nhttps://t.me/+5D0PteJ-XQ85Njll")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nYou are just 1 step away from joining 'POWER OF OPTION' official telegram channel, To Became a Profitable Trader 🎯🎯🚀🚀\n\nGet Daily FREE trading calls from POWER OF OPTION™ expert team having experience in Nifty, Bank Nifty, FinNifty, Commodities, Equities, Stock Option.\n\n✅ Trusted by 1,000+ traders\n\n✅ Receive 2-3 daily free trade calls and updates\n\n✅ All Trades comes with Proper Traget and Stoploss\n\n✅ Hand-Holding Support During Trades\n\n👇 Click on below button to join official 'POWER OF OPTION' telegram channel 👇\n\nhttps://t.me/+8iQYV1ueHYYwMzJl")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -44,7 +44,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\nYou are just 1 step away from joining 'AI TRADING BILLIONAIRE' official telegram channel 🎯🎯🚀🚀\n\nGet Daily FREE trading calls from our expert team having more than 11yrs experience in Nifty, Bank Nifty, FinNifty, Stock Option.\n\n✅ Trusted by 35,000+ traders\n\n✅ Receive 2-4 daily free trade calls and updates\n\n✅ More than 11yrs experience\n\n✅ Join for latest updates\n\n👇 Click on below button to join official 'AI TRADING BILLIONAIRE' telegram channel 👇\n\nhttps://t.me/+5D0PteJ-XQ85Njll")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\nYou are just 1 step away from joining 'POWER OF OPTION' official telegram channel, To Became a Profitable Trader 🎯🎯🚀🚀\n\nGet Daily FREE trading calls from POWER OF OPTION™ expert team having experience in Nifty, Bank Nifty, FinNifty, Commodities, Equities, Stock Option.\n\n✅ Trusted by 1,000+ traders\n\n✅ Receive 2-3 daily free trade calls and updates\n\n✅ All Trades comes with Proper Traget and Stoploss\n\n✅ Hand-Holding Support During Trades\n\n👇 Click on below button to join official 'POWER OF OPTION' telegram channel 👇\n\nhttps://t.me/+8iQYV1ueHYYwMzJl")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -56,7 +56,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only 'AI TRADING BILLIONAIRE' bot!"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only 'POWER OF OPTION' bot!"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
